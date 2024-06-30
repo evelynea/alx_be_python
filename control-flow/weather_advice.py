4dc73e6558e6f897d2cb4a3e9fb98e1d067cebd5
Evelyne <evelyneujeneza@gmail.com>
2024-06-30 14:34:01 +0200

    used if commands intead of match case

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 weather = input("what's the weather like today? (sunny/rainy/cold):")
-match weather:
-    case "sunny":
-        print("Wear a t-shirt and sunglasses.")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "cold":
-        print("Make sure to wear a warm coat and a scarf")
-    case _:
-        print("Sorry, I don't haverecommendations for this weather")
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+     print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+     print("Make sure to wear a warm coat and a scarf")
+else: 
+     print("Sorry, I don't haverecommendations for this weather")
