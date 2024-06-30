7afbfe1cee87603a2d421e75c594998979d1c901
Evelyne <evelyneujeneza@gmail.com>
2024-06-30 16:37:19 +0200

    adding conditional statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,8 +3,19 @@ priority = input ("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 print()
-match time_bound:
-    case "yes":
-        print(f"'{task}' is a high priority task that requires immediate attention today!")
-    case "no":
-        print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"'{task}' is a high priority task that requires immediate attention today!")
+        if time_bound == "no":
+            print(f"'{task}' is a high priority task but doesn't require immediate action")
+    case "medium":
+        if time_bound == "yes":
+            print(f"'{task}' is a medium priority task that requires immediate attention today!")
+        if time_bound == "no":
+            print(f"'{task}' is a medium priority task but doesn't require immediate action")
+    case "low":
+        if time_bound == "yes":
+            print(f"'{task}' is a low priority task that requires immediate attention today!")
+        if time_bound == "no":
+            print(f"'{task}' is a low priority task but doesn't require immediate action")
