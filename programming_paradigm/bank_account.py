806dc49eedf62b54ba13077427c015c026886426
Evelyne <evelyneujeneza@gmail.com>
2024-07-14 08:23:02 +0200

    bank account operations

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,19 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = account_balance
+        self.current_balance = account_balance
         account_balance = 0
 
     def deposit(self, amount):
-        return self.account_balance + amount
+        return self.current_balance + amount
         
 
     def withdraw(self, amount):
-        if self.account_balance - amount > 0:
+        if self.current_balance - amount > 0:
             return True  
         else:
             return False
 
 
     def display_balance(self):
-        print(f"The current balance on your account is {self.account_balance}")
+        print(f"The current balance on your account is {self.current_balance}")
 
